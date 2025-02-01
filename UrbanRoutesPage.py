8851fba5c800739b4dd1ec098e22c0e5093f410b
Crosty02 <crystal.puentes31@gmail.com>
2025-01-31 20:23:39 -0500

    URBAN ROUTES

--- UrbanRoutesPage.py
+++ UrbanRoutesPage.py
@@ -11,6 +11,8 @@ class UrbanRoutesPage:
         self.driver = driver
         self.wait = WebDriverWait(driver, 10)
 
+     # 1 Confirmación del establecimiento de la ruta
+
     def set_from(self, from_address):
      field = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(UrbanRoutesLocators.FROM_FIELD))
      field.clear()  # <-- Asegura que el campo esté vacío antes de escribir
@@ -29,11 +31,12 @@ class UrbanRoutesPage:
         field = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located(UrbanRoutesLocators.TO_FIELD))
         return field.get_attribute("value")
 
+    # 2 Prueba que verifica que se selecciona la tarifa Comfort
+
     def request_taxi(self):
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(UrbanRoutesLocators.REQUEST_TAXI_BUTTON)).click()
 
     def select_comfort_tariff(self):
-    # Esperar que el elemento sea clickable y hacer clic
         comfort_element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(UrbanRoutesLocators.COMFORT_TARIFF))
         comfort_element.click()
 
@@ -45,6 +48,7 @@ class UrbanRoutesPage:
         print(f"Clase del elemento después del clic: {element_class}")
         return "active" in element_class  # Verifica si tiene la clase "active"
 
+    # 3 Prueba que verifica que se agrega el número de teléfono y el código de confirmation
 
     def enter_phone_number(self, phone_number):
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(UrbanRoutesLocators.PHONE_FIELD_LABEL)).click()
@@ -79,9 +83,10 @@ class UrbanRoutesPage:
         return phone_input.get_attribute("value")  # Devuelve el número ingresado
 
 
+        # 4 Prueba que verifica que se agrega la tarjeta
+
     def add_card(self, card_number, card_code):
 
-            # Hacer clic en metodo de pago
             payment_button = self.wait.until(EC.element_to_be_clickable(UrbanRoutesLocators.PAYMENT_METHOD_BUTTON))
             payment_button.click()
 
@@ -105,6 +110,8 @@ class UrbanRoutesPage:
             close_button = self.wait.until(EC.presence_of_element_located(UrbanRoutesLocators.CLOSE_CARD_WINDOW_BUTTON))
             self.driver.execute_script("arguments[0].click();", close_button)
 
+     # 5 Prueba que verifica que se envia mensaje al conductor
+
     def enter_driver_message(self, message):
         # Hacer clic en el label para activar el campo
         WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(UrbanRoutesLocators.MESSAGE_LABEL)).click()
@@ -114,6 +121,7 @@ class UrbanRoutesPage:
         message_input.clear()
         message_input.send_keys(message)
 
+     # 6 Prueba que verifica que se pide la manta y los pañuelos
 
     def request_blanket_tissues(self):
         # Hacer clic en la etiqueta para seleccionar la opción
@@ -125,14 +133,15 @@ class UrbanRoutesPage:
         if not switch.is_selected():  # Verificar si ya está activado
          self.driver.execute_script("arguments[0].click();", switch)
 
+     # 7 Prueba que verifica que se pide los dos helados
 
     def request_two_ice_creams(self):
         plus_button = self.wait.until(EC.element_to_be_clickable(UrbanRoutesLocators.ICE_CREAM_PLUS_BUTTON))
-
-    # Hacer clic dos veces para pedir 2 helados
         plus_button.click()
         time.sleep(1)  # Pequeño delay en caso de animación
         plus_button.click()
+
+     # 8 Prueba que verifica que reserva el taxi
 
     def reserve_taxi(self):
         reserve_button = self.wait.until(EC.element_to_be_clickable(UrbanRoutesLocators.RESERVE_TAXI_BUTTON))
