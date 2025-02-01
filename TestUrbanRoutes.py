4fbe8fae128a84ce32c05511662a9b8f664af536
Crosty02 <crystal.puentes31@gmail.com>
2025-02-01 16:01:14 -0500

    URBAN ROUTES

--- TestUrbanRoutes.py
+++ TestUrbanRoutes.py
@@ -1,9 +1,7 @@
 import time
-
-from selenium.webdriver.common.by import By
-
 import data
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from UrbanRoutesPage import UrbanRoutesPage
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
